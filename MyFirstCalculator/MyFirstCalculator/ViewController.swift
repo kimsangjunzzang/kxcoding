71de9c2bcf51ee932a2c6cd263ba7ca31bc5dfea
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-06-12 22:33:58 +0900

    계산기 #9 -문법 최적화
    클로저 초기화

--- MyFirstCalculator/MyFirstCalculator/ViewController.swift
+++ MyFirstCalculator/MyFirstCalculator/ViewController.swift
@@ -28,8 +28,11 @@ class ViewController: UIViewController {
         let plusAction = UIAlertAction(title: "+ (더하기)", style: .default) { _  in
             self.operatorButton.setTitle("+", for: .normal)
         }
+//        let action = UIAlertAction(title: "+ (더하기)", style: .default, handler: {(action: UIAlertAction) -> Void in
+//            self.operatorButton.setTitle("+", for: .normal)
+//        })
         actionSheet.addAction(plusAction)
-        
+       
         let minusAction = UIAlertAction(title: "- (빼기)", style: .default) { _ in
             self.operatorButton.setTitle("-", for: .normal)
         }
@@ -52,7 +55,7 @@ class ViewController: UIViewController {
     
     @IBAction func calculate(_ sender: Any) {
         guard let text = firstOperandField.text, let a = Int(text) else{
-            showAlert("값을 입력해 주세요")
+            showAlert("값을 입력해 주세요","오류")
             return
         }
         
@@ -88,9 +91,9 @@ class ViewController: UIViewController {
         resultLabel.text = String(result)
     }
     
-    func showAlert(_ message : String){
-        let alert = UIAlertController(title: "알림", message: "\(message)", preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "확인", style: .default)
+    func showAlert(_ message : String,_ title: String = "알림"){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: title, style: .default)
         alert.addAction(okAction)
         present(alert, animated: true)
     }

0f5777676b2dd4e7e0ac0d486e219b1247a1aa57
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-06-17 22:57:45 +0900

    class vs Struct

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class Person {
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
+
+Person(name: "Name", age: 0)

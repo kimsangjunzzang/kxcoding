b9b871600b1c8076dbffb5b060fd0491a1607fda
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-06-19 01:35:42 +0900

    Table View #4 - 선택 이벤트
    테이블 뷰에서 발생하는 선택 이벤트를 처리합니다.

--- SimpleList/SimpleList/ViewController.swift
+++ SimpleList/SimpleList/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController{
     
     let fruits = ["Apple","Banana","Orange"]
+    let languages = ["Swift","Objective-C","C#","Java","HTML"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,26 +18,70 @@ class ViewController: UIViewController{
     
 }
 
-extension ViewController: UITableViewDataSource, UITableViewDelegate  {
+extension ViewController: UITableViewDataSource{
+    func numberOfSections(
+        in tableView: UITableView
+    ) -> Int {
+        print("#3",#function)
+        return 2
+    }
+    
     func tableView(
         _ tableView: UITableView,
         numberOfRowsInSection section: Int
     ) -> Int {
-        return fruits.count
+        print("#1",#function, section)
+        
+        switch section {
+        case 0:
+            return fruits.count
+        case 1:
+            return languages.count
+        default:
+            return 0
+        }
     }
     
     func tableView(
         _ tableView: UITableView,
         cellForRowAt indexPath: IndexPath
     ) -> UITableViewCell {
+        print("#2",#function, indexPath)
         // #1
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
         // #2
-        cell.textLabel?.text = fruits[indexPath.row]
-        
+        switch indexPath.section {
+        case 0:
+            cell.textLabel?.text = fruits[indexPath.row]
+        case 1:
+            cell.textLabel?.text = languages[indexPath.row]
+        default:
+            break
+            
+        }
         // #3
         return cell
+    }
+    
+}
+
+extension ViewController: UITableViewDelegate  {
+    
+    func tableView(
+        _ tableView: UITableView,
+        didSelectRowAt indexPath: IndexPath
+    ) {
+        switch indexPath.section {
+        case 0:
+            print(fruits[indexPath.row])
+        case 1:
+            print(languages[indexPath.row])
+        default:
+            break
+            
+        }
+        
     }
     
 }

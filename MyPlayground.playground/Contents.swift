57b4c5cc444e8f0e481608044c8491f5c774fed2
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-06-18 17:40:43 +0900

    Modeling rawdata

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -33,3 +33,37 @@ enum Alignment {
 }
 Alignment.center
 */
+
+//MARK: Rawvalue, 원시값은 변경이 불가능하다.
+
+/*
+ enum Alignment: Int{
+    case left
+    case right
+    case center
+}
+
+Alignment.left.rawValue // 0
+Alignment.right.rawValue // 1
+Alignment.center.rawValue // 2
+
+Alignment(rawValue: 0) // .left
+Alignment(rawValue: 200) // nil
+
+*/
+
+/*
+enum Weekday : String {
+    case sunday
+    case monday = "MON"
+    case tuesday
+    case wendnesday
+    case thursday
+    case firday
+    case saturday
+}
+
+Weekday.sunday.rawValue // sunday
+Weekday.monday.rawValue // MON
+Weekday.tuesday.rawValue // tuesday
+*/

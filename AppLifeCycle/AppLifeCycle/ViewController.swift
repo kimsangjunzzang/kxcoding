ddc002e36a791b8f4c194d29679b662f39eaec7f
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-06-23 22:37:24 +0900

    Application Life Cycle #1
    앱이 실행되는 과정을 자세히 공부합니다.

---
+++ AppLifeCycle/AppLifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppLifeCycle
+//
+//  Created by 김상준 on 6/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

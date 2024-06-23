8154342bb3c3fa1911502ee49f1f5c7d8543d15f
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-06-24 00:29:30 +0900

    Screen To View #1
    UI를 화면에 표시할 때 필요한 다양한 객체들을 소개합니다.

---
+++ ScreenToView/ScreenToView/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  ScreenToView
+//
+//  Created by 김상준 on 6/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print( UIScreen.main.bounds)
+        print (view.window?.windowScene?.screen.bounds)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print( UIScreen.main.bounds)
+        print (view.window?.windowScene?.screen.bounds)
+        
+        if let screen = view.window?.windowScene?.screen {
+            let width = screen.bounds.size.width * screen.scale // 스크린의 최적화 된 값을 구할 수 있다.
+            let height = screen.bounds.size.height * screen.scale
+            
+        }
+       
+    }
+    
+    @IBAction func test(_ sender: Any) {
+    }
+    
+}
+
+
+
+/*
+ 1. viewDidLoad는 뷰가 표시되기 전에 호출된다.
+ 2. 뷰를 표시할려면 window가 필요하다
+ 3. window에 뷰를 추가하면 화면에 표시된다.
+ 4. 뷰가 window에 추가되기 전까지는 화면에 표시되지 않고, window 속성에 접근하면 nil을 리턴한다.
+ 
+ 
+ View Controller 마다 Root View가 하나씩 있다.
+ Root View에는 view 속성으로 접근할 수 있다.
+ 스토리보드에서 추가하는 뷰는 Root View 아래쪽에 추가된다.
+ */

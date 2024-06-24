2144bbfe0a7f0c2c69e6070b927882fcc78daf60
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-06-24 23:37:55 +0900

    Segue #1 - Adaptive Segue
    Segue의 역할과 동작 방식을 공부합니다.

---
+++ HelloSegue/HelloSegue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloSegue
+//
+//  Created by 김상준 on 6/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

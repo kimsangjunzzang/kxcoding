98e7dec4f99f6dda62ab785ff5cc94147fb3e20b
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-06-27 23:40:02 +0900

    Constraint #7 - baseline, Relation
    baseline 정렬에 대해 공부하고 Relation의 역할을 공부합니다.

--- Constraint/Constraint/ViewController.swift
+++ Constraint/Constraint/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var leftEqualWidthConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var leftCenterXConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var rightCenterXConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var rightEqualWidthConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func selectLeft(_ sender: Any) {
+        leftCenterXConstraint.priority = UILayoutPriority.defaultHigh
+        leftEqualWidthConstraint.priority = .defaultHigh
+        
+        rightCenterXConstraint.priority = .defaultLow
+        rightEqualWidthConstraint.priority = .defaultLow
+        
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @IBAction func selectRight(_ sender: Any) {
+        
+        leftCenterXConstraint.priority = .defaultLow
+        leftEqualWidthConstraint.priority = .defaultLow
+        
+        rightCenterXConstraint.priority = .defaultHigh
+        rightEqualWidthConstraint.priority = .defaultHigh
+        
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
 }
 

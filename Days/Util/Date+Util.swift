a0fbbd5183476e663f878efdabe478af2fc53986
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-07-02 23:10:26 +0900

    Step #2 - Date Util
    익스텐션으로 Date 타입을 확장하고 반복적으로 사용하는 코드를 구현합니다.

---
+++ Days/Util/Date+Util.swift
@@ -0,0 +1,39 @@
+//
+//  Date+Util.swift
+//  Days
+//
+//  Created by 김상준 on 7/2/24.
+//
+
+import Foundation
+
+extension Date {
+    init(
+        year: Int,
+        month: Int,
+        day: Int,
+        hour: Int? = nil,
+        minute: Int? = nil,
+        second: Int? = nil
+    ) {
+        let calender = Calendar.current
+        
+        let components = DateComponents(year: year, month: month, day: day, hour: hour, minute: minute, second: second)
+        
+        self = calender.date(from: components) ?? Date(timeIntervalSinceReferenceDate: 0)
+    }
+    
+    func days(from date: Date) -> Int? {
+        let calendar = Calendar.current
+        
+        let from = calendar.startOfDay(for: date)
+        let to = calendar.startOfDay(for: self)
+        
+        return calendar.dateComponents([.day], from: from, to: to).day
+    }
+    
+    static var today: Date {
+        let calendar = Calendar.current
+        return calendar.startOfDay(for: .now)
+    }
+}
---
+++ Days/Util/Date.playground/Contents.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+let worldCup2002Date = Date(year: 2002,month: 5, day: 31)
+
+Date.today.days(from: worldCup2002Date)

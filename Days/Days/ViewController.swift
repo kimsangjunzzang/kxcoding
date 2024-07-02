68de7ca0948af644040d55dd579d77bfe59d5f53
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-07-02 21:51:39 +0900

    Step #1 - 이미지 다운로드, 프로젝트 생성
    프로젝트에서 사용하는 이미지를 다운로드하고 프로젝트를 생성합니다.

---
+++ Days/Days/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Days
+//
+//  Created by 김상준 on 7/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

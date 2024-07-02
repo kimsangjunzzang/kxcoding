264e8eb2fe56229a7926d8b0f95eff19dfd581e3
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-07-02 21:32:21 +0900

    사이즈 클래스

---
+++ AdaptiveLayout/AdaptiveLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdaptiveLayout
+//
+//  Created by 김상준 on 7/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

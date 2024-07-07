7f7a3ba52109de24b2278ecd6949888cec6e9aaa
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-07-08 08:56:47 +0900

    Step #3 - Time Zone
    테이블 뷰를 구현하고 TimeZone에 대해 공부합니다.

---
+++ AppleClock/AppleClock/Cell/WorldClockTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  WorldClockTableViewCell.swift
+//  AppleClock
+//
+//  Created by 김상준 on 7/8/24.
+//
+
+import UIKit
+
+class WorldClockTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var timeOffsetLabel: UILabel!
+    
+    @IBOutlet weak var timeZoneLabel: UILabel!
+    
+    @IBOutlet weak var timePeriodLabel: UILabel!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- AppleClock/AppleClock/WorldClockViewController.swift
+++ AppleClock/AppleClock/WorldClockViewController.swift
@@ -8,22 +8,37 @@
 import UIKit
 
 class WorldClockViewController: UIViewController {
-
+    
+    @IBOutlet weak var worldClockTableView: UITableView!
+    
+    var list = [
+        TimeZone(identifier: "Asia/Seoul")!,
+        TimeZone(identifier: "Europe/Paris")!
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        print(TimeZone.knownTimeZoneIdentifiers.count)
+        for tz in TimeZone.knownTimeZoneIdentifiers {
+            print(tz)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+extension WorldClockViewController: UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: WorldClockTableViewCell.self),for: indexPath) as! WorldClockTableViewCell
+        
+        return cell
+    }
+    
+    
 }

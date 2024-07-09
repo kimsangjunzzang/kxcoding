e1c7f5c6023fab4dc4da3fd38c57c2551aba9d66
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-07-09 23:11:10 +0900

    Step #1 - 리소스, 프로젝트
    프로젝트에서 사용하는 리소스를 다운로드하고 프로젝트를 구성합니다.

---
+++ PlanetPedia/PlanetPedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlanetPedia
+//
+//  Created by 김상준 on 7/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

31093cb0feaad12a8c77aacb44a32ad9511dc132
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-08-21 11:21:47 +0900

    Memo App Step #8 - Relative Date Formatting
    날짜를 상대적인 문자열로 포멧팅 합니다.

--- Memo/Memo/ListViewController.swift
+++ Memo/Memo/ListViewController.swift
@@ -8,27 +8,27 @@
 import UIKit
 
 class ListViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         DataManager.shared.fetch()
     }
-
-
 }
 
 extension ListViewController : UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int
+    ) -> Int {
         return DataManager.shared.list.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
         let target = DataManager.shared.list[indexPath.row]
         cell.textLabel?.text = target.content
-        cell.detailTextLabel?.text = target.insertDate?.description
+        cell.detailTextLabel?.text = target.dateString
         
         return cell
     }
--- Memo/Memo/Model/MemoEntity+CoreDataClass.swift
+++ Memo/Memo/Model/MemoEntity+CoreDataClass.swift
@@ -9,7 +9,18 @@
 import Foundation
 import CoreData
 
+fileprivate let formatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .medium
+    formatter.timeStyle = .none
+    formatter.doesRelativeDateFormatting = true // 날짜를 오늘 이나 어제로 표현 가능합니다.
+    return formatter
+}()
+
 @objc(MemoEntity)
 public class MemoEntity: NSManagedObject {
+    var dateString: String? {
+        return formatter.string(for: insertDate)
+    }
 
 }

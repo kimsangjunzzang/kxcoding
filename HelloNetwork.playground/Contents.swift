95af523863e9f31d12364b3a7f6398d682d9fbdc
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-08-30 16:00:03 +0900

    step2
    step2

--- HelloNetwork.playground/Contents.swift
+++ HelloNetwork.playground/Contents.swift
@@ -3,14 +3,7 @@ import UIKit
 let url = URL(string: "https://kxapi.azurewebsites.net/helloworld?apiKey=4TKFcdetA8Xw3NWVEgMd")!
 
 url.scheme
-// http, https(http + TLS)
-
-// ftp
-// ftps
-// sftp
-
-// ssh
-
+// http, https(http + TLS), ftp, ftps, sftp, ssh
 url.host(percentEncoded: true)
 
 url.port // 논리적인 포트 0 ~ 65535, well-known port
@@ -22,5 +15,31 @@ url.pathComponents
 url.query(percentEncoded: true)
 // ㅋㅣ = 값
 
+let session = URLSession.shared // 싱글톤 객체
+
+let task = session.dataTask(with: url) { data, response, error in
+    if let error {
+        // 경고창, 에러 화면으로 이동
+        print(error)
+        return
+    }
+    
+    guard let httpResponse = response as? HTTPURLResponse else {
+        return
+    }
+    
+    guard httpResponse.statusCode == 200 else {
+        return
+    }
+    
+    guard let data else {
+        return
+    }
+    
+    let str = String(data: data, encoding: .utf8)
+    print(str)
+}
+
+task.resume() // task 실행
               
               

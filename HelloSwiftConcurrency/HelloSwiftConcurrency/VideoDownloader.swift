f71a0e433e02647a3211c3571180a1d5b21d75e3
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-10-04 00:25:21 +0900

    Simple Video Player #2
    기존 코드의 문제점을 분석하고 Swift Concurrency로 개선하는 방법을 공부합니다.

--- HelloSwiftConcurrency/HelloSwiftConcurrency/VideoDownloader.swift
+++ HelloSwiftConcurrency/HelloSwiftConcurrency/VideoDownloader.swift
@@ -22,6 +22,10 @@ class VideoDownloader {
     }
     
     // 백그라운드에서 비동기로 다운로드
+    // 문제점
+    // 1. 에러를 직접 보내지 못하니깐 Result 타입에 담아서 보내야한다.
+    // 2. 값을 바로 리턴하는 것도 불가능 하다.
+    // 3. completion을 호출하는 코드를 빼먹어도 컴파일러가 인식하지 못한다 (가장 큰 문제점이다.)
     func download(completion: @escaping (Result<URL, Error>) -> ()) {
         DispatchQueue.global().async {
             do {
@@ -37,6 +41,18 @@ class VideoDownloader {
                 }
             }
         }
+    }
+    
+    // 동기 메서드 처럼 보이나 비동기로 작동 되고 있는 코드이다.
+    func download() async throws -> URL { // async method
+        let (data,response) = try await URLSession.shared.data(from: url)
+        // await = suspension point, 일시중단 지점
+        
+        guard let httpUrlResponse = response as? HTTPURLResponse, httpUrlResponse.statusCode == 200 else {
+            throw DounloadError.error
+        }
+        try data.write(to: localUrl, options: .atomic)
+        return localUrl
     }
 }
 
--- HelloSwiftConcurrency/HelloSwiftConcurrency/ViewController.swift
+++ HelloSwiftConcurrency/HelloSwiftConcurrency/ViewController.swift
@@ -13,42 +13,63 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultLabel: UILabel!
     
     let downloader = VideoDownloader()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func download(_ sender: Any) {
         resultLabel.text = "다운로드 중..."
         
-        downloader.download { [weak self] result in
-            switch result {
-                case .success(let url):
+        // #3
+        // 동기 메서드 안에서 async 메서드를 직접 호출 할 수 없다.
+        // 호출 하고 싶다면, Task를 사용해야 해야한다.
+        Task {
+            do {
+                let url = try await downloader.download()
+                resultLabel.text = "다운로드 성공"
+                
                 let vc = AVPlayerViewController()
                 vc.player = AVPlayer(url: url)
                 
-                self?.present(vc, animated: true) {
+                present(vc, animated: true) {
                     vc.player?.play()
                 }
-            case .failure(let error):
-                self?.resultLabel.text = error.localizedDescription
+            } catch {
+                resultLabel.text = error.localizedDescription
             }
         }
         
-//        do {
-//            let url = try downloader.download()
-//            resultLabel.text = "다운로드 성공"
-//            
-//            let vc = AVPlayerViewController()
-//            vc.player = AVPlayer(url: url)
-//            
-//            present(vc, animated: true) {
-//                vc.player?.play()
-//            }
-//        } catch {
-//            resultLabel.text = error.localizedDescription
-//        }
+        // # 2
+        //        downloader.download { [weak self] result in
+        //            switch result {
+        //                case .success(let url):
+        //                let vc = AVPlayerViewController()
+        //                vc.player = AVPlayer(url: url)
+        //
+        //                self?.present(vc, animated: true) {
+        //                    vc.player?.play()
+        //                }
+        //            case .failure(let error):
+        //                self?.resultLabel.text = error.localizedDescription
+        //            }
+        //        }
+        
+        // #1
+        //        do {
+        //            let url = try downloader.download()
+        //            resultLabel.text = "다운로드 성공"
+        //
+        //            let vc = AVPlayerViewController()
+        //            vc.player = AVPlayer(url: url)
+        //
+        //            present(vc, animated: true) {
+        //                vc.player?.play()
+        //            }
+        //        } catch {
+        //            resultLabel.text = error.localizedDescription
+        //        }
         
     }
     
